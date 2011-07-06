33f32fdf945204c37fd9ebc79e0aa7a93d4cc722
John Medforth <john.medforth@blacksquaremedia.com>
2011-07-06 15:25:30 +0100

    oops. double paste.

--- lib/devise_imap_authenticatable/adapter.rb
+++ lib/devise_imap_authenticatable/adapter.rb
@@ -6,15 +6,13 @@ module Devise
   module ImapAdapter
 
     def self.valid_credentials?(username, password)
-      def self.valid_credentials?(username, password)
-        imap = Net::IMAP.new(::Devise.imap_server)
-        imap.authenticate("cram-md5", username, password)
-        true
-      rescue Net::IMAP::ResponseError => e
-        false
-      ensure
-        imap.disconnect
-      end
+      imap = Net::IMAP.new(::Devise.imap_server)
+      imap.authenticate("cram-md5", username, password)
+      true
+    rescue Net::IMAP::ResponseError => e
+      false
+    ensure
+      imap.disconnect
     end
 
   end
