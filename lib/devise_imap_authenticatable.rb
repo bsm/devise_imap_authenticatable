23f0d2c722623b59baa6b202bb3c82ecdc940bb3
John Medforth <john.medforth@blacksquaremedia.com>
2011-07-06 14:18:55 +0100

    Minore updates.

--- lib/devise_imap_authenticatable.rb
+++ lib/devise_imap_authenticatable.rb
@@ -6,6 +6,13 @@ require 'devise_imap_authenticatable/version'
 require 'devise_imap_authenticatable/imap_adapter'
 
 
+module Devise
+  # imap server address for authentication.
+  mattr_accessor :imap_server
+  @@imap_server = nil
+
+end
+
 # Add ldap_authenticatable strategy to defaults.
 #
 Devise.add_module(:imap_authenticatable,
