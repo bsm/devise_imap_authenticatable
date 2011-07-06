c2ba6e626be8906bdf440dd3bb1d5879c37a4d6c
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2011-07-06 16:52:25 +0100

    Updated model spec

--- spec/devise_imap_authenticatable/model_spec.rb
+++ spec/devise_imap_authenticatable/model_spec.rb
@@ -2,14 +2,13 @@ require 'spec_helper'
 
 describe Devise::Models::ImapAuthenticatable do
 
-  let :subject do
-    User.new
+  subject do
+    User.new :email => 'tom@example.com'
   end
 
   it 'should validate a users password' do
-    subject.email = 'someemail@domain.com'
-    Devise::ImapAdapter.should_receive(:valid_credentials?).with('someemail@domain.com', 'password').and_return(true)
-    subject.valid_password?('password').should be_true
+    Devise::ImapAdapter.should_receive(:valid_credentials?).with('tom@example.com', 'secret').and_return(true)
+    subject.valid_password?('secret').should be_true
   end
 
 end
